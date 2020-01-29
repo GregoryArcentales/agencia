db16b61944c40eba91303c84e1237929b6b6575d
Alex Gregory Arcentales Bailon <alexarcentales25@gmail.com>
2020-01-29 11:43:50 -0500

    .

--- public/js/app.js
+++ public/js/app.js
@@ -37149,8 +37149,8 @@ if (btnGasto) {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! c:\xampp\htdocs\php-projects\agencia\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! c:\xampp\htdocs\php-projects\agencia\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\php-projects\agencia\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\php-projects\agencia\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
